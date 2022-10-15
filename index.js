7b8b4b03ccadd0676e7cabf9f67015d509d2ac6c
martha ochoa <marthaeochoa98@gmail.com>
2022-10-15 15:34:16 -0500

    commit tree 80ecf6ab9eafc7e23480d8e1c74439619963ac60

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const block = 42
+const feet = 264
+
+function distanceFromHqInBlocks(distance){
+    if(distance > block){
+        return distance - block;
+    } else if (distance < block){
+        return block - distance;
+    }
+}
+
+function distanceFromHqInFeet(distance){
+    return distanceFromHqInBlocks(distance) * feet
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (start < destination){
+        return (destination - start) * feet
+    } else {
+        return (start - destination) * feet
+    }
+}
